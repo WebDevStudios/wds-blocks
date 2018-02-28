eff43d18eef326a9c2b0f8e03f23877273489405
Unknown <coreymcollins@gmail.com>
2018-02-28 12:52:03 -0600

    Accidentally reset my branch :facepalm: -- updating it again

--- src/blocks/fifty-fifty-block/index.js
+++ src/blocks/fifty-fifty-block/index.js
@@ -7,9 +7,21 @@ const { __ } = wp.i18n;
 const {
 	AlignmentToolbar,
 	BlockControls,
+	BlockDescription,
+	InspectorControls,
+	MediaUpload,
 	registerBlockType,
 	RichText,
 } = wp.blocks;
+
+const {
+	Button,
+	Dashicon,
+	FormToggle,
+	PanelBody,
+	PanelRow,
+	SelectControl,
+} = wp.components;
 
 // Register block.
 export default registerBlockType(
@@ -40,6 +52,28 @@ export default registerBlockType(
 			alignmentRight: {
 				type: 'string',
 			},
+			imgURL: {
+				type: 'string',
+				source: 'attribute',
+				attribute: 'src',
+				selector: 'img',
+			},
+			imgID: {
+				type: 'number',
+			},
+			imgAlt: {
+				type: 'string',
+				source: 'attribute',
+				attribute: 'alt',
+				selector: 'img',
+			},
+			layout: {
+				type: 'string',
+			},
+			columnOrder: {
+				type: 'boolean',
+				default: false,
+			},
 		},
 		edit: props => {
 			// Change the Left message value as we type.
@@ -62,71 +96,296 @@ export default registerBlockType(
 				props.setAttributes( { alignmentRight: value } );
 			};
 
-			return (
-				<section className={ props.className }>
+			// Select an image.
+			const onSelectImage = img => {
+				props.setAttributes( {
+					imgID: img.id,
+					imgURL: img.url,
+					imgAlt: img.alt,
+				} );
+			};
+
+			// Remove an image.
+			const onRemoveImage = () => {
+				props.setAttributes( {
+					imgID: null,
+					imgURL: null,
+					imgAlt: null,
+				} );
+			};
+
+			// Toggle our layout.
+			const onChangeLayout = value => {
+				props.setAttributes( { layout: value } );
+			};
+
+			// Toggle our column order.
+			const toggleColumnOrder = () => {
+				props.setAttributes( { columnOrder: ! props.attributes.columnOrder } );
+			};
+
+			function displayLeftMessage() {
+				return (
+					<div className="content-block-content content-block">
+						<h2>{ __( 'Column Text' ) }</h2>
+
+						{
+							!! props.focus && (
+								<BlockControls key="controlsLeft">
+									<AlignmentToolbar
+										value={ props.attributes.alignmentLeft }
+										onChange={ onChangeAlignmentLeft }
+									/>
+								</BlockControls>
+							)
+						}
+						<RichText
+							tagName="div"
+							multiline="p"
+							className="content-block-left"
+							style={ { textAlign: props.attributes.alignmentLeft } }
+							placeholder={ __( 'Enter your content here for one column of the Two-Column block' ) }
+							onChange={ onChangeMessageLeft }
+							value={ props.attributes.messageLeft }
+							focus={ props.focus }
+							onFocus={ props.setFocus }
+						/>
+					</div>
+				);
+			}
+
+			function displayRightMessage() {
+				return (
+					<div className="content-block-content content-block">
+						<h2>{ __( 'Column Text' ) }</h2>
+
+						{
+							!! props.focus && (
+								<BlockControls key="controlsRight">
+									<AlignmentToolbar
+										value={ props.attributes.alignmentRight }
+										onChange={ onChangeAlignmentRight }
+									/>
+								</BlockControls>
+							)
+						}
+						<RichText
+							tagName="div"
+							multiline="p"
+							className="content-block-right"
+							style={ { textAlign: props.attributes.alignmentRight } }
+							placeholder={ __( 'Enter your content here for one column of the Two-Column block' ) }
+							onChange={ onChangeMessageRight }
+							value={ props.attributes.messageRight }
+							focus={ props.focus }
+							onFocus={ props.setFocus }
+						/>
+					</div>
+				);
+			}
+
+			function displayMediaUpload() {
+				return (
+					! props.attributes.imgID ? (
+						<MediaUpload
+							buttonProps={ {
+								className: 'components-button button button-large',
+							} }
+							onSelect={ onSelectImage }
+							type="image"
+							value={ props.attributes.imgID }
+							render={ ( { open } ) => (
+								<Button className="button button-large" onClick={ open }>
+									<Dashicon icon="format-image" /> { ! props.attributes.imgID ? __( 'Upload Image' ) : <img src={ props.attributes.imgURL } alt={ props.attributes.imgAlt } /> }
+								</Button>
+							) }
+						/>
+					) : (
+						<p className="image-wrapper">
+							<img
+								src={ props.attributes.imgURL }
+								alt={ props.attributes.imgAlt }
+							/>
+							{ props.focus ? (
+								<Button
+									className="remove-image button button-large"
+									onClick={ onRemoveImage }
+								>
+									<Dashicon icon="no-alt" /> { __( 'Remove Image' ) }
+								</Button>
+							) : null }
+						</p>
+					)
+				);
+			}
+
+			function displayLayoutFields() {
+				if ( 'text-image' === props.attributes.layout ) {
+					return [
+						displayLeftMessage(),
+						displayMediaUpload(),
+					];
+				} else if ( 'image-text' === props.attributes.layout ) {
+					return [
+						displayMediaUpload(),
+						displayLeftMessage(),
+					];
+				} else if ( 'text-text' === props.attributes.layout || ! props.attributes.layout ) {
+					// If the toggle is clicked, display the Right column first.
+					if ( props.attributes.columnOrder ) {
+						return [
+							displayRightMessage(),
+							displayLeftMessage(),
+						];
+					}
+
+					// Otherwise, display the columns as usual.
+					return [
+						displayLeftMessage(),
+						displayRightMessage(),
+					];
+				}
+			}
+
+			return [
+				!! props.focus && (
+					<InspectorControls key="inspector">
+
+						<BlockDescription>
+							<p>{ __( 'Some options for your nifty Fifty Fifty!' ) }</p>
+						</BlockDescription>
+
+						<PanelBody title={ __( 'Fifty Fifty Options Panel' ) }>
+
+							<PanelRow>
+								<SelectControl
+									key="layout"
+									label={ __( 'Layout' ) }
+									value={ props.attributes.layout ? props.attributes.layout : '' }
+									options={ [
+										{
+											label: __( 'Text/Text' ),
+											value: 'text-text',
+										},
+										{
+											label: __( 'Text/Image' ),
+											value: 'text-image',
+										},
+										{
+											label: __( 'Image/Text' ),
+											value: 'image-text',
+										},
+									] }
+									onChange={ onChangeLayout }
+								/>
+							</PanelRow>
+
+							{
+								'text-text' === props.attributes.layout || ! props.attributes.layout ? (
+									<PanelRow>
+										<label
+											htmlFor="column-order"
+											className="blocks-base-control__label"
+										>
+											{ __( 'Switch Text Column Order' ) }
+										</label>
+
+										<FormToggle
+											id="column-order"
+											label={ __( 'Column Order' ) }
+											checked={ !! props.attributes.columnOrder }
+											onChange={ toggleColumnOrder }
+										/>
+									</PanelRow>
+								) : (
+									null
+								)
+							}
+
+						</PanelBody>
+
+					</InspectorControls>
+				),
+				<section key={ props.className } className={ props.className }>
 
 					<header className="content-block-header">
 						<h2>{ __( 'Two-Column Block' ) }</h2>
 					</header>
 
 					<div className="content-block-container">
-
-						<div className="content-block-content content-block-left">
-							<h2>{ __( 'Left Column' ) }</h2>
-
-							{
-								!! props.focus && (
-									<BlockControls key="controlsLeft">
-										<AlignmentToolbar
-											value={ props.attributes.alignmentLeft }
-											onChange={ onChangeAlignmentLeft }
-										/>
-									</BlockControls>
-								)
-							}
-							<RichText
-								tagName="div"
-								multiline="p"
-								className="content-block-left"
-								style={ { textAlign: props.attributes.alignmentLeft } }
-								placeholder={ __( 'Enter your content here for the left Two-Column block' ) }
-								onChange={ onChangeMessageLeft }
-								value={ props.attributes.messageLeft }
-								focus={ props.focus }
-								onFocus={ props.setFocus }
-							/>
-						</div>
-
-						<div className="content-block-content content-block-right">
-							<h2>{ __( 'Right Column' ) }</h2>
-
-							{
-								!! props.focus && (
-									<BlockControls key="controlsRight">
-										<AlignmentToolbar
-											value={ props.attributes.alignmentRight }
-											onChange={ onChangeAlignmentRight }
-										/>
-									</BlockControls>
-								)
-							}
-							<RichText
-								tagName="div"
-								multiline="p"
-								className="content-block-right"
-								style={ { textAlign: props.attributes.alignmentRight } }
-								placeholder={ __( 'Enter your content here for the right Two-Column block' ) }
-								onChange={ onChangeMessageRight }
-								value={ props.attributes.messageRight }
-								focus={ props.focus }
-								onFocus={ props.setFocus }
-							/>
-						</div>
+						{ displayLayoutFields() }
 					</div>
-				</section>
-			);
+				</section>,
+			];
 		},
 		save: props => {
+			function displayLeftMessageOutput() {
+				return (
+					<div
+						key="content-block"
+						className="content-block-content content-block-left"
+						style={ { textAlign: props.attributes.alignmentLeft } }
+					>
+						{ props.attributes.messageLeft }
+					</div>
+				);
+			}
+
+			function displayRightMessageOutput() {
+				return (
+					<div
+						key="content-block"
+						className="content-block-content content-block-right"
+						style={ { textAlign: props.attributes.alignmentRight } }
+					>
+						{ props.attributes.messageRight }
+					</div>
+				);
+			}
+
+			function displayImageOutput() {
+				return (
+					<div
+						key="content-block-image"
+						className="content-block-content content-block"
+						style={ { textAlign: props.attributes.alignmentRight } }
+					>
+						<img
+							src={ props.attributes.imgURL }
+							alt={ props.attributes.imgAlt }
+						/>
+					</div>
+				);
+			}
+
+			function displayLayoutOutput() {
+				if ( 'text-image' === props.attributes.layout ) {
+					return [
+						displayLeftMessageOutput(),
+						displayImageOutput(),
+					];
+				} else if ( 'image-text' === props.attributes.layout ) {
+					return [
+						displayImageOutput(),
+						displayLeftMessageOutput(),
+					];
+				} else if ( 'text-text' === props.attributes.layout || ! props.attributes.layout ) {
+					// If the toggle is clicked, display the Right column first.
+					if ( props.attributes.columnOrder ) {
+						return [
+							displayRightMessageOutput(),
+							displayLeftMessageOutput(),
+						];
+					}
+
+					// Otherwise, display the columns as usual.
+					return [
+						displayLeftMessageOutput(),
+						displayRightMessageOutput(),
+					];
+				}
+			}
+
 			return (
 				<section className="content-block grid-container two-column">
 
@@ -135,19 +394,7 @@ export default registerBlockType(
 					</header>
 
 					<div className="content-block-container">
-
-						<div
-							className="content-block-content content-block-left"
-							style={ { textAlign: props.attributes.alignmentLeft } }
-						>
-							{ props.attributes.messageLeft }
-						</div>
-						<div
-							className="content-block-content content-block-right"
-							style={ { textAlign: props.attributes.alignmentRight } }
-						>
-							{ props.attributes.messageRight }
-						</div>
+						{ displayLayoutOutput() }
 					</div>
 				</section>
 			);
