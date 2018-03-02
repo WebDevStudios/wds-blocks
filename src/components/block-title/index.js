fa6843a593b736b219f001616194ecceed112729
Unknown <coreymcollins@gmail.com>
2018-03-02 13:33:03 -0600

    Updates placeholder text to be customizable

--- src/components/block-title/index.js
+++ src/components/block-title/index.js
@@ -38,7 +38,7 @@ export default class BlockTitle extends Component {
 						) }
 						value={ ! this.props.attributes.blockTitle ? null : this.props.attributes.blockTitle }
 						onChange={ this.onChangeBlockTitle }
-						placeholder={ __( 'Two-Column Content Block Section Title (optional)' ) }
+						placeholder={ this.props.placeholder ? this.props.placeholder : __( 'Two-Column Content Block Section Title (optional)' ) }
 					/>
 				</h2>
 			</header>
