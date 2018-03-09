756a246140a4c96892788388392855fdb3dd7a5a
Corey Collins <coreymcollins@gmail.com>
2018-03-09 10:28:30 -0600

    Moves title block into a render file within the component

---
+++ src/components/block-title/render.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Helper function for displaying the block title.
+ *
+ * @package WDS_Gutenberg
+ * @since NEXT
+ */
+
+namespace WDS_Gutenberg\Src\WDS_Component;
+
+/**
+ * Displays the Block Title for the section.
+ *
+ * @param  object $attributes Object containing our $attribute values.
+ * @return empty              Return nothing if there is no title.
+ *
+ * @author Corey Collins
+ *
+ * @since NEXT
+ */
+function display_block_title( $attributes ) {
+	if ( ! $attributes['blockTitle'] ) {
+		return;
+	}
+
+	?>
+	<header className="content-block-header">
+		<h2><?php echo esc_html( $attributes['blockTitle'] ); ?></h2>
+	</header>
+	<?php
+}
--- src/template-tags/block-container-options.php
+++ src/template-tags/block-container-options.php
@@ -83,25 +83,3 @@ function display_block_options( $attributes ) {
 		echo $background_video_markup; // WPCS XSS OK.
 	}
 }
-
-/**
- * Displays the Block Title for the section.
- *
- * @param  object $attributes Object containing our $attribute values.
- * @return empty              Return nothing if there is no title.
- *
- * @author Corey Collins
- *
- * @since NEXT
- */
-function display_block_title( $attributes ) {
-	if ( ! $attributes['blockTitle'] ) {
-		return;
-	}
-
-	?>
-	<header className="content-block-header">
-		<h2><?php echo esc_html( $attributes['blockTitle'] ); ?></h2>
-	</header>
-	<?php
-}
