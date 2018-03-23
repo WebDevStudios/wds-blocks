33e1dc7c444ece47d25600fad4dd028dffb3a8b5
jomurgel <dev@jomurgel.com>
2018-03-23 17:37:02 -0600

    Rolls search component into edit component and adds scroll Event listender to posts ul to fetch posts on scroll

--- src/blocks/new-related-posts/edit.js
+++ src/blocks/new-related-posts/edit.js
@@ -2,6 +2,7 @@ const { Component, Fragment } = wp.element;
 
 import Output from './output';
 import Loader from '../../components/loader';
+import Search from '../../components/search';
 
 // Import our Block Title component.
 import BlockTitle from '../../components/block-title';
@@ -11,13 +12,21 @@ class EditComponent extends Component {
 		super( ...props );
 
 		this.state = {
+			page: 1,
 			allPosts: [],
 			selectedPosts: [],
+			queriedPosts: [],
 			isLoaded: false,
+			atEnd: false,
+			totalPosts: '',
 		};
 
+		this.apiURL = ( page ) => wpApiSettings.root + `wp/v2/posts?page=${ page }&per_page=5`;
+
 		this.handleEvent = this.handleEvent.bind( this );
+		this.fetchData = this.fetchData.bind( this );
 		this.returnLayout = this.returnLayout.bind( this );
+		this.returnQueryData = this.returnQueryData.bind( this );
 	}
 
 	handleEvent = clickedPost => {
@@ -34,30 +43,55 @@ class EditComponent extends Component {
 		} );
 	};
 
-	componentDidMount() {
-		const apiURL = wpApiSettings.root + 'wp/v2/posts?per_page=100';
+	returnQueryData( responseQuery ) {
+		this.setState( { queriedPosts: responseQuery } );
+	}
 
-		return window.fetch( apiURL )
+	fetchData( page ) {
+		this.setState( { isLoaded: false } );
+
+		window.fetch( this.apiURL( page ) )
 			.then( response => {
-				response.json().then( data => ( {
-					data: data,
-					status: response.status,
-				} ) ).then( res => {
-					if ( res.status === 200 ) {
-						this.setState( { allPosts: res.data, isLoaded: true } );
-					}
+				if ( response.status === 200 ) {
+					const total = parseInt( response.headers.get( 'X-WP-Total' ) );
+					this.setState( { totalPosts: total } );
+					return response.json();
+				}
+				this.setState( { atEnd: true } );
+				return [];
+			} )
+			.then( response => {
+				this.setState( {
+					page: page,
+					allPosts: this.state.allPosts.concat( response ),
+					isLoaded: true,
 				} );
 			} );
+	}
+
+	componentDidMount() {
+		if ( this.state.page === 1 && this.state.allPosts.length === 0 && this.state.queriedPosts.length === 0 ) {
+			this.fetchData( this.state.page );
+		}
+
+		this.container.addEventListener( 'scroll', () => {
+			if ( this.state.allPosts.length !== this.state.totalPosts && ( this.container.scrollTop === ( this.container.scrollHeight - this.container.offsetHeight ) ) ) {
+				this.fetchData( this.state.page + 1 );
+			} else {
+				this.setState( { atEnd: true } );
+			}
+		} );
 	}
 
 	returnLayout = () => {
 		return (
 			<Fragment>
 				<Output
+					textRef={ ( element ) => this.container = element }
 					title="Posts"
 					className="related-left-column"
 					key=""
-					posts={ this.state.allPosts }
+					posts={ this.state.queriedPosts.length > 0 ? this.state.queriedPosts : this.state.allPosts }
 					handleEvent={ this.handleEvent }
 				/>
 				<Output
@@ -77,6 +111,7 @@ class EditComponent extends Component {
 			<BlockTitle key=""
 				{ ...this.props }
 			/>,
+			<Search key="" onQueryChange={ this.returnQueryData } />,
 			<div key="" className={ this.props.className }>
 				{ ! this.state.isLoaded ? <Loader key="" /> : null }
 				{ this.returnLayout() }
