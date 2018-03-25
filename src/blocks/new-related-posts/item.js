87004f9e5610df3677ce39b2c853fc876f442f99
jomurgel <dev@jomurgel.com>
2018-03-25 13:32:14 -0600

    adds an `is-selected` class to item if ... selected ...

--- src/blocks/new-related-posts/item.js
+++ src/blocks/new-related-posts/item.js
@@ -1,7 +1,18 @@
 const Item = ( props ) => {
-	const { handleClick, post, postName } = props;
+	const { handleClick, post, postName, isActive } = props;
+
+	function className() {
+		// If current post is in selectedPosts.
+		if ( post && undefined !== isActive ) {
+			if ( isActive.indexOf( post ) > -1 ) {
+				return post.slug + ' is-selected';
+			}
+			return post.slug;
+		}
+	}
+
 	/* eslint-disable */
-	return <li onClick={ () => handleClick( post ) }> { postName } </li>;
+	return <li className={ className() } onClick={ ( ( e ) => handleClick( { e, post } ) ) }> { postName } </li>;
 }
 
 export default Item;
