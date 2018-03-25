4a19efcd023f6342afc2821f32d74d78a1c96592
jomurgel <dev@jomurgel.com>
2018-03-25 15:36:23 -0600

    adds selected class to item if item also exists in selected posts array

--- src/blocks/new-related-posts/item.js
+++ src/blocks/new-related-posts/item.js
@@ -1,12 +1,14 @@
 const Item = ( props ) => {
 	const { handleClick, post, postName, isActive } = props;
 
+	// Set classname if post.id exists in selected posts.
 	function className() {
 		// If current post is in selectedPosts.
 		if ( post && undefined !== isActive ) {
-			if ( isActive.indexOf( post ) > -1 ) {
+			if ( isActive.filter( e => e.id === post.id ).length > 0 ) {
 				return post.slug + ' is-selected';
 			}
+
 			return post.slug;
 		}
 	}
