53bc45baed2dbd22a28d7a2ce7135223cd75cc13
jomurgel <dev@jomurgel.com>
2018-04-16 15:11:26 -0600

    adds JSON placeholder to state

--- src/blocks/new-related-posts/edit.js
+++ src/blocks/new-related-posts/edit.js
@@ -14,6 +14,7 @@ class EditComponent extends Component {
 		this.state = {
 			page: 1,
 			allPosts: [],
+			selectedPostsJSON: '[]',
 			selectedPosts: [],
 			queriedPosts: [],
 			isLoaded: false,
