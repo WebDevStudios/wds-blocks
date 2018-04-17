da837d80baafbe1053ae5bc0a0e53beb7600ebb1
Eric Fuller <info@glassmoonmedia.com>
2018-04-17 17:09:57 -0400

    Refactor the handleChange and handleClick functions. Also pulling in tags and categories now.

--- src/components/multiselect/index.js
+++ src/components/multiselect/index.js
@@ -11,62 +11,32 @@ class MultiSelect extends Component {
 	constructor( props ) {
 		super( props );
 
+		this.APIRootURL = wpApiSettings.root + `wp/v2/`;
+
 		this.state = {
 			selectedItems: [],
 			expanded: true,
-			inputValue: props.value,
+			tags: [],
+			categories: [],
+			inputValue: props.value
 		};
 	}
 
-	handleChange = ( event ) => {
-		const selectedItems = this.state.selectedItems;
-		const value = event.target.value;
+	componentDidMount() {
+		if ( this.props.tags ) {
+			const url = this.APIRootURL + `tags`;
 
-		if ( selectedItems.length === 0 ) {
-			selectedItems.push( value );
-			this.setState( {
-				selectedItems: selectedItems,
-			} );
-		} else {
-			for ( let i = 0; i < selectedItems.length; i++ ) {
-				if ( value === selectedItems[ i ] ) {
-					selectedItems.splice( i, 1 );
-					this.setState( {
-						selectedItems: selectedItems,
-					} );
-					return;
-				}
-			}
-			selectedItems.push( value );
-			this.setState( {
-				selectedItems: selectedItems,
-			} );
+			window.fetch( url )
+				.then( res => res.json() )
+				.then( tags => this.setState({ tags }) );
 		}
-	}
 
-	handleClick = ( event ) => {
-		const selectedItems = this.state.selectedItems;
-		const value = event.target.innerText;
+		if ( this.props.categories ) {
+			const url = this.APIRootURL + `categories`;
 
-		if ( selectedItems.length === 0 ) {
-			selectedItems.push( value );
-			this.setState( {
-				selectedItems: selectedItems,
-			} );
-		} else {
-			for ( let i = 0; i < selectedItems.length; i++ ) {
-				if ( value === selectedItems[ i ] ) {
-					selectedItems.splice( i, 1 );
-					this.setState( {
-						selectedItems: selectedItems,
-					} );
-					return;
-				}
-			}
-			selectedItems.push( value );
-			this.setState( {
-				selectedItems: selectedItems,
-			} );
+			window.fetch( url )
+				.then( res => res.json() )
+				.then( categories => this.setState({ categories }) );
 		}
 	}
 
@@ -80,14 +50,34 @@ class MultiSelect extends Component {
 		return status;
 	}
 
+	handleChange = ( item ) => () => {
+		const inSelected = this.state.selectedItems.filter( selectedItem => selectedItem.id === item.id );
+
+		if ( 0 < inSelected.length ) {
+			return;
+		}
+
+		this.setState( ( prevState ) => {
+			return {
+				selectedItems: [ ...prevState.selectedItems, item ]
+			}
+		} );
+	};
+
+	handleInputClick = ( item ) => () => {
+		const newSelected = this.state.selectedItems.filter( selectedItem => selectedItem.id !== item.id );
+
+		this.setState({ selectedItems: newSelected });
+	};
+
 	renderDropdown = () => {
-		const inputValue = this.state.inputValue;
+		const inputValue = this.state.tags;
 
 		return (
-			inputValue.map( ( item, index ) => (
-				<Fragment key={ `${ item }-${ index } ` }>
-					<input id={ item } type="checkbox" value={ item } onChange={ this.handleChange } ref={ input => this[ `checkbox${ item }` ] = input } checked={ this.checkStatus( item ) } />
-					<label htmlFor={ item }>{ item }</label>
+			inputValue.map( ( item ) => (
+				<Fragment key={ `${ item.name }-${ item.id } ` }>
+					<input id={ item.id } type="checkbox" value={ item.name } onChange={ this.handleChange( item ) } ref={ input => this[ `checkbox${ item.name }` ] = input } checked={ this.checkStatus( item ) } />
+					<label htmlFor={ item.id }>{ item.name }</label>
 				</Fragment>
 			) )
 		);
@@ -103,10 +93,10 @@ class MultiSelect extends Component {
 		const items = this.state.selectedItems;
 		return (
 			items.length > 0 ?
-				items.map( ( item, index ) => (
+				items.map( ( item ) => (
 					/* eslint-disable */
-					<span key={ `${ item }-${ index } ` } className="item-pill" onClick={ this.handleClick }>
-						{ item }
+					<span key={ `${ item.name }-${ item.id } ` } className="item-pill" onClick={ this.handleInputClick( item ) }>
+						{ item.name }
 					</span>
 					/* eslint-enable */
 				) ) :
