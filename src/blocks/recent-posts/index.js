b8d2e5252b3498fd0c12040952da2d2c30d219b6
jomurgel <dev@jomurgel.com>
2018-04-18 10:57:54 -0600

    adds recent posts block copied from core gutenberg latest posts block

--- src/blocks/recent-posts/index.js
+++ src/blocks/recent-posts/index.js
@@ -15,9 +15,7 @@ import classnames from 'classnames'; // Import NPM libraries here.
  */
 const { __ } = wp.i18n;
 const {
-	InspectorControls,
 	registerBlockType,
-	RichText,
 } = wp.blocks;
 
 /**
@@ -25,14 +23,7 @@ const {
  */
 import './style.scss';
 
-// Import all of our Background Options requirements.
-import BackgroundOptions, { BackgroundOptionsAttributes, BackgroundOptionsClasses, BackgroundOptionsInlineStyles, BackgroundOptionsVideoOutput } from '../../components/background-options';
-
-// Import all of our Text Options requirements.
-import TextOptions, { TextOptionsAttributes, TextOptionsInlineStyles } from '../../components/text-options';
-
-// Import all of our Other Options requirements.
-import OtherOptions, { OtherOptionsAttributes, OtherOptionsClasses } from '../../components/other-options';
+import RecentPostsBlock from './recent-posts';
 
 /**
  * Register block
@@ -43,156 +34,29 @@ import OtherOptions, { OtherOptionsAttributes, OtherOptionsClasses } from '../..
  * @return {?WPBlock}          The block, if it has been successfully
  *                             registered; otherwise `undefined`.
  */
-export default registerBlockType( 'wds/wds-recent-posts', { // Namespaced with 'wds/', lowercase, hyphenated.
+export default registerBlockType( 'wds/recent-posts', { // Namespaced with 'wds/', lowercase, hyphenated.
 	// Localize title using wp.i18n.__()
 	title: __( 'WDS Recent Posts' ),
 	// Description: Write a quick description.
-	description: __( 'Select Recent Posts.' ),
+	description: __( 'Shows a list of your site\'s most recent posts.' ),
 	// Category options: common, formatting, layout, widgets, embed.
-	category: 'common',
+	category: 'widgets',
 	// Can use a Dashicon (see https://developer.wordpress.org/resource/dashicons/) or an imported SVG.
-	icon: 'sos',
+	icon: 'clock',
 	// Limit to 3 keywords/phrases. Users will see your block when they search using these keywords.
 	keywords: [
-		__( 'Options' ),
-		__( 'Editable' ),
-		__( 'Multiline' ),
+		__( 'recent posts' ),
 	],
-	// Set for each piece of dynamic data used in your block.
-	// https://wordpress.org/gutenberg/handbook/block-api/attributes/
-	attributes: {
-		message: {
-			type: 'array',
-			source: 'children',
-			selector: '.content-block',
-		},
-		...BackgroundOptionsAttributes,
-		...TextOptionsAttributes,
-		...OtherOptionsAttributes,
+	supports: {
+		html: false,
 	},
 	// Determines what is displayed in the editor.
 	// https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/#edit
-	edit: props => {
-		// Event handler to update the value of the content when changed in editor.
-		const setMessageAttribute = value => {
-			props.setAttributes( { message: value } );
-		};
-		// Return the markup displayed in the editor, including a core Editable field.
-		return [
-			!! props.focus && (
-				<InspectorControls key="inspector">
-					{ BackgroundOptions( props ) }
-					{ TextOptions( props ) }
-					{ OtherOptions( props ) }
-				</InspectorControls>
-			),
-			<section
-				key="editable-content-example-block-with-options"
-				className={ classnames(
-					props.className,
-					...BackgroundOptionsClasses( props ),
-					...OtherOptionsClasses( props ),
-				) }
-				style={ {
-					...BackgroundOptionsInlineStyles( props ),
-					...TextOptionsInlineStyles( props ),
-				} }
-			>
-
-				{ BackgroundOptionsVideoOutput( props ) }
-
-				<header className="content-block-header">
-					<h2
-						style={ {
-							color: props.attributes.textColor ? props.attributes.textColor : null,
-						} }
-					>
-						{ __( 'WDS Default Block' ) }
-					</h2>
-				</header>
-
-				<RichText
-					tagName="div"
-					multiline="p"
-					className="content-block"
-					placeholder={ __( 'To customize this block, click on "Show Advanced Settings"' ) }
-					onChange={ setMessageAttribute }
-					value={ props.attributes.message }
-					focus={ props.focus }
-					onFocus={ props.setFocus }
-				/>
-			</section>,
-		];
-	},
+	edit: RecentPostsBlock,
 	// Determines what is displayed on the front-end.
 	// https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/#save
 	//
 	// For dynamic blocks, you can return null here and define a render callback function in PHP.
 	// https://wordpress.org/gutenberg/handbook/blocks/creating-dynamic-blocks/
-	save: props => {
-		return (
-			<section
-				className={ classnames(
-					props.className,
-					...BackgroundOptionsClasses( props ),
-					...OtherOptionsClasses( props ),
-				) }
-				style={ {
-					...BackgroundOptionsInlineStyles( props ),
-					...TextOptionsInlineStyles( props ),
-				} }
-			>
-
-				{ BackgroundOptionsVideoOutput( props ) }
-
-				<header className="content-block-header">
-					<h2>{ __( 'WDS Example Block with Options' ) }</h2>
-				</header>
-
-				<div className="content-block-content content-block">
-					{ props.attributes.message }
-				</div>
-			</section>
-		);
-	},
-	deprecated: [
-		{
-			attributes: {
-				message: {
-					type: 'array',
-					source: 'children',
-					selector: '.content-block',
-				},
-				...BackgroundOptionsAttributes,
-				...TextOptionsAttributes,
-				...OtherOptionsAttributes,
-			},
-			save( props ) {
-				return (
-					<section
-						className={ classnames(
-							props.className,
-							...BackgroundOptionsClasses( props ),
-							...OtherOptionsClasses( props ),
-						) }
-						style={ {
-							...BackgroundOptionsInlineStyles( props ),
-							...TextOptionsInlineStyles( props ),
-						} }
-					>
-
-						{ BackgroundOptionsVideoOutput( props ) }
-
-						<header className="content-block-header">
-							<h2>{ __( 'WDS Example Block with Options' ) }</h2>
-						</header>
-
-						<div className="content-block-content content-block">
-							{ props.attributes.message }
-						</div>
-					</section>
-				);
-			},
-		},
-	],
+	save: () => null,
 } );
