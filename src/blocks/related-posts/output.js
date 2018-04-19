58e5b8b22a504db364ce1f7a7b92bbf8e78dfae4
jomurgel <dev@jomurgel.com>
2018-04-19 15:01:56 -0600

    converts to stateless component

--- src/blocks/related-posts/output.js
+++ src/blocks/related-posts/output.js
@@ -1,39 +1,31 @@
 import Item from './item';
 
-const { Component } = wp.element;
+const Output = ( props ) => {
+	const {
+		activeClass,
+		className,
+		handleEvent,
+		posts,
+		title,
+		textRef,
+	} = props;
 
-class Output extends Component {
-	constructor( props ) {
-		super( ...props );
-	}
-
-	render() {
-		const {
-			activeClass,
-			className,
-			handleEvent,
-			posts,
-			title,
-			textRef,
-		} = this.props;
-
-		return (
-			<div className={ className } tabIndex="0">
-				<h3 key="">{ title }</h3>
-				<ul ref={ textRef }>
-					{ posts.map( ( result, index ) => (
-						<Item
-							isActive={ activeClass }
-							key={ `${ result.id }-${ index }` }
-							postName={ result.title.rendered }
-							post={ result }
-							handleClick={ handleEvent }
-						/>
-					) ) }
-				</ul>
-			</div>
-		);
-	}
-}
+	return (
+		<div className={ className } tabIndex="0">
+			<h3 key="">{ title }</h3>
+			<ul ref={ textRef }>
+				{ posts.map( ( result, index ) => (
+					<Item
+						isActive={ activeClass }
+						key={ `${ result.id }-${ index }` }
+						postName={ result.title.rendered }
+						post={ result }
+						handleClick={ handleEvent }
+					/>
+				) ) }
+			</ul>
+		</div>
+	);
+};
 
 export default Output;
