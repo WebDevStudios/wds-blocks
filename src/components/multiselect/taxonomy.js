20a4310466dca72e9db34d3c9e33840a03aa61f0
Eric Fuller <info@glassmoonmedia.com>
2018-04-24 17:35:16 -0400

    Handle the name and selected items props.

--- src/components/multiselect/taxonomy.js
+++ src/components/multiselect/taxonomy.js
@@ -11,14 +11,17 @@ import InputPanel from './input';
 import DropDown from './dropdown';
 
 const Taxonomy = ( props ) => {
-	const { selectedItems, items, handleChange, handleInputClick } = props;
+	const { name, selectedItems, items, handleChange, handleInputClick } = props;
+
 	return (
 		<Fragment>
 			<InputPanel
 				selectedItems={ selectedItems }
 				handleInputClick={ handleInputClick }
+				name={ name }
 			/>
 			<DropDown
+				selectedItems={ selectedItems }
 				items={ items }
 				handleChange={ handleChange }
 			/>
