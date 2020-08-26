a4b25260072efd0ec5f811a8d3f698321f432746
R A Van Epps <rebekah.vanepps@webdevstudios.com>
2020-08-26 16:48:58 -0600

    Apply color class/style on save

--- src/blocks/carousel-slide/save.js
+++ src/blocks/carousel-slide/save.js
@@ -1,4 +1,4 @@
-import { InnerBlocks } from '@wordpress/block-editor';
+import { getColorClassName, InnerBlocks } from '@wordpress/block-editor';
 
 /**
  * The save function defines the way in which the different attributes should
@@ -13,10 +13,20 @@ import { InnerBlocks } from '@wordpress/block-editor';
  * @return {WPElement}      Element to render.
  */
 export default function Save( props ) {
-	const { attributes, className } = props;
+	const { attributes: { fontColor, customFontColor }, className } = props;
+
+	const classes = [ className ],
+		styles = {};
+
+	// Add custom color classes.
+	classes.push( fontColor || customFontColor ? 'has-text-color' : null );
+	classes.push( fontColor ? getColorClassName( 'color', fontColor ) : null );
+
+	// Add custom color styles.
+	styles.color = customFontColor ? customFontColor : undefined;
 
 	return (
-		<div className={ className }>
+		<div className={ classes.filter( Boolean ).join( ' ' ) } style={ styles }>
 			<InnerBlocks.Content />
 		</div>
 	);
