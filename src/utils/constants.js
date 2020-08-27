ac5c81e9c5ce2e7a0d575e99b45cf13c0a39cb22
Greg Rickaby <greg@gregrickaby.com>
2020-08-27 07:31:27 -0500

    run through prettier

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,25 +5,28 @@ export const PREFIX = 'wdsblocks';
 
 // Container Class (.container, .row, etc).
 export const CONTAINER_CLASS = applyFilters(
-	`${PREFIX}.container_class`,
+	`${ PREFIX }.container_class`,
 	`container`
 );
 
 // Theme Background Palette.
-export const THEME_BKG_PALETTE = applyFilters(`${PREFIX}.theme_bkg_palette`, [
-	{
-		name: 'Lavender',
-		slug: 'lavender',
-		color: '#E6E6FA',
-	},
-	{
-		name: 'Blue',
-		slug: 'blue',
-		color: '#6c97e0',
-	},
-	{
-		name: 'White',
-		slug: 'white',
-		color: '#ffffff',
-	},
-]);
+export const THEME_BKG_PALETTE = applyFilters(
+	`${ PREFIX }.theme_bkg_palette`,
+	[
+		{
+			name: 'Lavender',
+			slug: 'lavender',
+			color: '#E6E6FA',
+		},
+		{
+			name: 'Blue',
+			slug: 'blue',
+			color: '#6c97e0',
+		},
+		{
+			name: 'White',
+			slug: 'white',
+			color: '#ffffff',
+		},
+	]
+);
