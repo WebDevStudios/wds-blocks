b0d938f06a5df6fa5ab5bfbb4f8a1d4335be3c01
R A Van Epps <rebekah.vanepps@webdevstudios.com>
2020-09-04 11:59:11 -0600

    Add carousel settings filter

--- src/blocks/carousel/utils/config.js
+++ src/blocks/carousel/utils/config.js
@@ -1,12 +1,23 @@
+import { applyFilters } from '@wordpress/hooks';
 import { PREFIX } from '../../../utils/config';
 
-// Carousel settings.
-export const GLIDE_SETTINGS = {
-	autoplay: 5000,
-	focusAt: 'center',
-	gap: 0,
-	type: 'carousel',
-};
+/**
+ * Filter (GlideJS) carousel settings.
+ *
+ * @author WebDevStudios
+ * @since  2.0.0
+ *
+ * @param  {Object} [settings] Carousel settings.
+ */
+export const GLIDE_SETTINGS = applyFilters(
+	`${ PREFIX }.carouselSlide.carouselSettings`,
+	{
+		autoplay: 5000,
+		focusAt: 'center',
+		gap: 0,
+		type: 'carousel',
+	}
+);
 
 // Carousel InnerBlocks props.
 export const INNER_BLOCKS_PROPS = {
