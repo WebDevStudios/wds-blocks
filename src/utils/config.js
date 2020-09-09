9a857b52d833fc858f43d743f4b3757e3f8e5153
R A Van Epps <rebekah.vanepps@webdevstudios.com>
2020-09-09 10:44:09 -0600

    Remove custom color palette config

--- src/utils/config.js
+++ src/utils/config.js
@@ -7,30 +7,3 @@ export const CONTAINER_CLASS = applyFilters(
 	`${ PREFIX }.container_class`,
 	`container`
 );
-
-// Theme Background Palette
-export const THEME_BKG_PALETTE = applyFilters(
-	`${ PREFIX }.theme_bkg_palette`,
-	[
-		{
-			name: 'Coral',
-			slug: 'coral',
-			color: '#FF7F50',
-		},
-		{
-			name: 'Lavender',
-			slug: 'lavender',
-			color: '#E6E6FA',
-		},
-		{
-			name: 'Blue',
-			slug: 'blue',
-			color: '#6c97e0',
-		},
-		{
-			name: 'White',
-			slug: 'white',
-			color: '#ffffff',
-		},
-	]
-);
