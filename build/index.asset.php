4f98a95b722270048239c6281cdc0573a4e318f2
Greg Rickaby <gregrickaby@gmail.com>
2021-06-28 13:46:10 -0500

    check-in /build dir

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => 'ec75f55b76d5415a236790e97ab6145f');
